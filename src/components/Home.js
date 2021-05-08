4c18a2ea55ba064dcfe9fdcb4e78f2951907943e
unknown <dsouza.jovin65@gmail.com>
2021-05-08 10:31:10 +0530

    Error removed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import ImageUpload from './ImageUpload';
-import ImageList from './ImageList';
-import Users from './Users.js';
-import MyImages from './MyImages';
-import { auth } from '../firebase';
+// import ImageUpload from './ImageUpload';
+// import ImageList from './ImageList';
+// import Users from './Users.js';
+// import MyImages from './MyImages';
+// import { auth } from '../firebase';
 import { useStateValue } from '../StateProvider';
 import { actionTypes } from '../reducer';
 import Layout from './Layout/Layout';
 
 const Home = () => {
-  const [state, dispatch] = useStateValue();
+  // const [state, dispatch] = useStateValue();
 
-  const signOutUser = () => {
-    console.log('Inside Log');
-    // auth.signOut();
-    dispatch({
-      type: actionTypes.LOGOUT,
-    });
-  };
+  // const signOutUser = () => {
+  //   console.log('Inside Log');
+  //   // auth.signOut();
+  //   dispatch({
+  //     type: actionTypes.LOGOUT,
+  //   });
+  // };
 
   return (
     <>
