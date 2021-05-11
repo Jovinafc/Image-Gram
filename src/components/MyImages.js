b791dbdcb6b597114b81226400466f8bcfcea45f
unknown <dsouza.jovin65@gmail.com>
2021-05-11 21:17:46 +0530

    Added Profile

--- src/components/MyImages.js
+++ src/components/MyImages.js
@@ -8,11 +8,14 @@ const MyImages = () => {
   const [{ user }, dispatch] = useStateValue();
   const [myPosts, setMyPosts] = useState([]);
 
+  console.log(user);
   useEffect(() => {
     if (user) {
-      db.collection(`users/${user.uid}/posts`)
+      db.collection('posts')
+        .where('uid', '==', user.uid)
         .orderBy('timestamp', 'desc')
-        .onSnapshot((snapshot) =>
+        .get()
+        .then((snapshot) =>
           setMyPosts(
             snapshot.docs.map((doc) => ({
               id: doc.id,
@@ -20,19 +23,41 @@ const MyImages = () => {
             }))
           )
         );
+
+      // db.collection(`users/${user.uid}/posts`)
+      //   .orderBy('timestamp', 'desc')
+      //   .onSnapshot((snapshot) =>
+      //     setMyPosts(
+      //       snapshot.docs.map((doc) => ({
+      //         id: doc.id,
+      //         data: doc.data(),
+      //       }))
+      //     )
+      //   );
     }
   }, [user]);
 
-  return (
+  return user ? (
     <div className='myImages'>
-      <h3>My Images</h3>
+      <div className='myImages__header'>
+        <div className='myImages__left'>
+          <img className='myImages__img' src={user.photoURL} />
+        </div>
+        <div className='myImages__right'>
+          <p className='myImages__username'>{user.displayName}</p>
+
+          <p>{myPosts.length} posts</p>
+        </div>
+      </div>
 
       <div className='myImages__Cont'>
         {myPosts.map((post, index) => (
-          <MyImage key={post.id} post={post.data} />
+          <MyImage key={post.id} id={post.id} post={post.data} />
         ))}
       </div>
     </div>
+  ) : (
+    <div>Loading...</div>
   );
 };
 
--- src/components/Post/MyImage.js
+++ src/components/Post/MyImage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './MyImage.css';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
-const MyImage = ({ key, post }) => {
+const MyImage = ({ id, post }) => {
   return (
-    <div key={key} className='myImage'>
+    <div key={id} className='myImage'>
       <img className='myImage__img' src={post.imageUrl} />
-      <div className='myImage__overlay'>Likes</div>
+      <div className='myImage__overlay'>
+        <FavoriteIcon />
+        <p>{post.likes ? post.likes.length : 0}</p>
+      </div>
     </div>
   );
 };
